dd7031382f24c0588a3df8412a2dbfd98bcb4248
“LauraGA777” <laura_garciaa@soy.sena.edu.co>
2024-09-16 19:13:37 -0500

    Mejorar componente PrivateRoute para redirigir a la página de inicio de sesión si el usuario no está autenticado

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,10 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { UseLoginContext } from "../Components/Context/LoginContext";
+
+const PrivateRoute = ({ children }) => {
+
+    const {isLogin} = useContext(UseLoginContext);
+    return isLogin ? children : <Navigate to="/login" />;
+};
+export default PrivateRoute;
